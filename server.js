618804956f278bbb349bcfa92bd5687d65e36020
Anna Vasileva <vasilec1992@yandex.rum>
2021-02-16 11:38:28 +0100

    change port

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ const server = http.createServer((req, res) => {
     }
   });
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 server.listen(port, "localhost", () => {
   console.log(`Listening on port ${port}`);
 });
